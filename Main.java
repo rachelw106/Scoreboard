987e4d463b964ca3ce9d48b4f5e90dcee243bc57
rachelw106 <rachelw106@nycstudents.net>
2024-10-01 13:32:42 +0000

    commit

--- Main.java
+++ Main.java
@@ -10,5 +10,26 @@ public class Main
         game.recordPlay(1);
         info = game.getScore();
         System.out.println(info);
+        
+        game.recordPlay(0);
+        info = game.getScore();
+        System.out.println(info);
+
+        info = game.getScore();
+        System.out.println(info);
+        
+        game.recordPlay(3);
+        info = game.getScore();
+        System.out.println(info);
+        
+        game.recordPlay(1);
+        game.recordPlay(0);
+        
+        info = game.getScore();
+        System.out.println(info);
+        
+        game.recordPlay(0);
+        info = game.getScore();
+        System.out.println(info);
     }
 }
--- Scoreboard.java
+++ Scoreboard.java
@@ -1,30 +1,26 @@
-public class Scoreboard 
-{
+public class Scoreboard {
     private String teamOne;
     private String teamTwo;
     private int teamOneScore;
     private int teamTwoScore;
     private String activeTeam;
+    public int swap = 0;
 
-    public Scoreboard(String one, String two)
-    {
+    public Scoreboard(String one, String two) {
         teamOne = one;
         teamTwo = two;
         activeTeam = teamOne;
     }
 
-    public String getScore()
-    {
+    public void recordPlay(int score) {
+        if (score > 0 && activeTeam == teamOne) teamOneScore = teamOneScore + score;
+        else if (score < 1 && activeTeam == teamOne) activeTeam = teamTwo;
+        else if (score > 0 && activeTeam == teamTwo) teamTwoScore = teamTwoScore + score;
+        else if (score < 1 && activeTeam == teamTwo) activeTeam = teamOne;
+    }
+
+    public String getScore() {
         return teamOneScore + "-" + teamTwoScore + "-" + activeTeam;
     }
 
-    public void recordPlay(int score)
-    {
-        if(score < 1) activeTeam = teamTwo;
-        else
-        {
-            teamOneScore = teamOneScore + score;
-        }
-    }
-    
 }
